74a4e25f3a9eacf8b21ea1c4025ece6a3398bf9a
Mijojac <mijojac@gmail.com>
2021-08-20 10:58:12 +0200

    Initial Commit

---
+++ Atmo/AtmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtmoApp.swift
+//  Atmo
+//
+//  Created by Miroslav Mali on 20.8.21..
+//
+
+import SwiftUI
+
+@main
+struct AtmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atmo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Atmo
+//
+//  Created by Miroslav Mali on 20.8.21..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
