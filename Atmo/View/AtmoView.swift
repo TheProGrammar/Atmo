90c92768ce2dd0dc56e71afcdc105813d01805c5
TheProGrammar <mixlaproduction@gmail.com>
2021-08-23 14:29:18 +0200

    Add simple animated splash screen

--- Atmo/View/AtmoView.swift
+++ Atmo/View/AtmoView.swift
@@ -10,21 +10,52 @@ import SwiftUI
 struct AtmoView: View {
     
     @ObservedObject var viewModel: WeatherViewModel
+    @State var animate = false
+    @State var endSplash = false
     
     var body: some View {
-        NavigationView {
-            VStack {
-                Text(viewModel.city)
-                Text(viewModel.temp)
-                Text(viewModel.description)
-                Image(viewModel.weatherImage)
+        
+        ZStack {
+            
+            Home(viewModel: viewModel)
+            
+            ZStack {
+                
+                Color("blue")
+                
+                Image("clipart")
                     .resizable()
-                    .frame(width: 100, height: 100)
+                    .renderingMode(.original)
+                    .aspectRatio(contentMode: animate ? .fill : .fit)
+                    .frame(width: animate ? nil : 85, height: animate ? nil : 85)
+                
+                    // Scaling View...
+                    .scaleEffect(animate ? 3 : 1)
+                    // setting width to avoid over size...
+                    .frame(width: UIScreen.main.bounds.width)
             }
-            .navigationTitle("Atmo")
+            .ignoresSafeArea(.all, edges: .all)
+            .onAppear(perform: animateSplash)
+            // hiding view after finishing...
+            .opacity(endSplash ? 0 : 1)
         }
-        .onAppear() {
-            viewModel.refresh()
+    }
+    
+    func animateSplash() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+            
+            withAnimation(Animation.easeOut(duration: 1)) {
+                
+                animate.toggle()
+                
+            }
+            
+            withAnimation(Animation.easeOut(duration: 1)) {
+                
+                endSplash.toggle()
+                
+            }
         }
     }
 }
@@ -34,3 +65,27 @@ struct ContentView_Previews: PreviewProvider {
         AtmoView(viewModel: WeatherViewModel(weatherService: WeatherService())) 
     }
 }
+
+struct Home : View {
+    
+    @ObservedObject var viewModel: WeatherViewModel
+    
+    var body: some View {
+        
+        VStack {
+
+            Text(viewModel.city)
+            Text(viewModel.temp)
+            Text(viewModel.description)
+            Image(viewModel.weatherImage)
+                .resizable()
+                .frame(width: 100, height: 100)
+        }
+        .onAppear() {
+            viewModel.refresh()
+        }
+        
+    }
+}
+
+
